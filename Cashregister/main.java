474aa1ac03aada70779b4f3a693815a6adf98c42
picazoandre-louis <42677897+picazoandre-louis@users.noreply.github.com>
2019-03-20 21:21:57 -0700

    Create main.java

---
+++ Cashregister/main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        ArrayList<Product> inventory = new ArrayList<Product>();
+        inventory.add(new Product(1, 2.99, true, "Guava"));
+    }
+}
