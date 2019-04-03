f4ba11639ed126aaa5743a64234955ad6a276bb1
picazoandre-louis <42677897+picazoandre-louis@users.noreply.github.com>
2019-04-02 22:45:27 -0700

    Create Frigate.java

---
+++ Starship'd/Frigate.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+public class Frigate extends Starship implements isDefense {
+
+    public Frigate(String newName){
+        Random rand = new Random();
+        hull = rand.nextInt(501)+1500;
+        shield = rand.nextInt(501)+2000;
+        beam = rand.nextInt(1001)+2500;
+        torp = rand.nextInt(11)+20;
+        name = newName;
+    }
+
+    public void showStats(){
+        System.out.println(name + ":   Hull: " + hull + "   Shield: " + shield + "   Beam: " + beam + "   Torpedoes: " + torp );
+    }
+}
