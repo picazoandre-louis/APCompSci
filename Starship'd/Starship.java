2e11b28abd8a78e468264adbd2173b13b263295c
picazoandre-louis <42677897+picazoandre-louis@users.noreply.github.com>
2019-04-02 22:44:36 -0700

    Create Starship.java

---
+++ Starship'd/Starship.java
@@ -0,0 +1,13 @@
+
+public abstract class Starship {
+    protected int hull;
+    protected int shield;
+    protected int beam;
+    protected int torp;
+    protected String name;
+
+    public void showStats(){
+
+    }
+
+}
